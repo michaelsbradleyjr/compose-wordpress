f4f34fc409ac3ed9ec5e1339771591c799e02887
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2015-09-10 16:19:34 -0500

    helper scripts should check for docker-machine command

--- scripts/machine-create-vb.sh
+++ scripts/machine-create-vb.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+command -v docker-machine &>/dev/null
+if [ $? != 0 ]; then
+    echo "This script requires the docker-machine command"
+    exit 1
+fi
+
 if [ -z "$1" ]; then
     echo "Please provide a name for the machine"
     exit 1
