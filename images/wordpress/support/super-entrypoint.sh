bd8e62888a465dffae09e11c01c4ca7df7061d28
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2015-09-11 01:36:55 -0500

    super-entrypoint.sh wip

--- images/wordpress/support/super-entrypoint.sh
+++ images/wordpress/support/super-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -e
 
-# test if $1 is "apache2-foreground", if so then fire it up, otherwise, assume the user wants
-# to do something altogether different
-
-/postfix-install.sh
-/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+if [ "$1" == apache2* ]; then
+    /postfix-install.sh
+    /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+else
+    exec "$@"
+fi
