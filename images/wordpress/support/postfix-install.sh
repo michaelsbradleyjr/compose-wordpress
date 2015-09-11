1f2fcf2d0d67404061858020dd58e374a112ba73
Michael Bradley, Jr <michaelsbradleyjr@gmail.com>
2015-09-11 01:36:32 -0500

    postfix-install.sh wip

--- images/wordpress/support/postfix-install.sh
+++ images/wordpress/support/postfix-install.sh
@@ -1,130 +1,41 @@
 #!/bin/bash
 
-#judgement
-if [[ -a /etc/supervisor/conf.d/supervisord.conf ]]; then
+# Adapted from:
+# https://github.com/catatnight/docker-postfix
+
+# ---------
+# judgement
+# ---------
+if [ -e /etc/supervisor/conf.d/supervisord.conf ]; then
   exit 0
 fi
 
-#supervisor
-cat > /etc/supervisor/conf.d/supervisord.conf <<EOF
-[supervisord]
-nodaemon=true
+# ----------
+# supervisor
+# ----------
+cp /support/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
 
-[program:postfix]
-command=/opt/postfix.sh
-
-[program:rsyslog]
-command=/usr/sbin/rsyslogd -n -c3
-EOF
-
-############
-#  postfix
-############
-cat >> /opt/postfix.sh <<EOF
-#!/bin/bash
-service postfix start
-tail -f /var/log/mail.log
-EOF
-chmod +x /opt/postfix.sh
+# -------
+# postfix
+# -------
+maildomain=$WP_MAIL_DOMAIN
+if [ -z "$maildomain" ]; then
+    maildomain="mail.example.com"
+fi
+mailorigin=$WP_MAIL_ORIGIN
+if [ -z "$mailorigin" ]; then
+    mailorigin="example.com"
+fi
+echo "root,wordpress" > /etc/postfix/canonical
+chmod 644 /etc/postfix/canonical
+postconf -e canonical_maps=hash:/etc/postfix/canonical
+postconf -e inet_interfaces=localhost
+postconf -e masquerade_domains=$mailorigin
+postcont -e mydestination="localhost, localhost.localdomain, localhost"
 postconf -e myhostname=$maildomain
+postconf -e mynetworks="127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128"
+postconf -e myorigin=$mailorigin
 postconf -F '*/*/chroot = n'
 
-############
-# SASL SUPPORT FOR CLIENTS
-# The following options set parameters needed by Postfix to enable
-# Cyrus-SASL support for authentication of mail clients.
-############
 # /etc/postfix/main.cf
-postconf -e smtpd_sasl_auth_enable=yes
-postconf -e broken_sasl_auth_clients=yes
-postconf -e smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination
-# smtpd.conf
-cat >> /etc/postfix/sasl/smtpd.conf <<EOF
-pwcheck_method: auxprop
-auxprop_plugin: sasldb
-mech_list: PLAIN LOGIN CRAM-MD5 DIGEST-MD5 NTLM
-EOF
-# sasldb2
-echo $smtp_user | tr , \\n > /tmp/passwd
-while IFS=':' read -r _user _pwd; do
-  echo $_pwd | saslpasswd2 -p -c -u $maildomain $_user
-done < /tmp/passwd
-chown postfix.sasl /etc/sasldb2
-
-############
-# Enable TLS
-############
-if [[ -n "$(find /etc/postfix/certs -iname *.crt)" && -n "$(find /etc/postfix/certs -iname *.key)" ]]; then
-  # /etc/postfix/main.cf
-  postconf -e smtpd_tls_cert_file=$(find /etc/postfix/certs -iname *.crt)
-  postconf -e smtpd_tls_key_file=$(find /etc/postfix/certs -iname *.key)
-  chmod 400 /etc/postfix/certs/*.*
-  # /etc/postfix/master.cf
-  postconf -M submission/inet="submission   inet   n   -   n   -   -   smtpd"
-  postconf -P "submission/inet/syslog_name=postfix/submission"
-  postconf -P "submission/inet/smtpd_tls_security_level=encrypt"
-  postconf -P "submission/inet/smtpd_sasl_auth_enable=yes"
-  postconf -P "submission/inet/milter_macro_daemon_name=ORIGINATING"
-  postconf -P "submission/inet/smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination"
-fi
-
-#############
-#  opendkim
-#############
-
-if [[ -z "$(find /etc/opendkim/domainkeys -iname *.private)" ]]; then
-  exit 0
-fi
-cat >> /etc/supervisor/conf.d/supervisord.conf <<EOF
-
-[program:opendkim]
-command=/usr/sbin/opendkim -f
-EOF
-# /etc/postfix/main.cf
-postconf -e milter_protocol=2
-postconf -e milter_default_action=accept
-postconf -e smtpd_milters=inet:localhost:12301
-postconf -e non_smtpd_milters=inet:localhost:12301
-
-cat >> /etc/opendkim.conf <<EOF
-AutoRestart             Yes
-AutoRestartRate         10/1h
-UMask                   002
-Syslog                  yes
-SyslogSuccess           Yes
-LogWhy                  Yes
-
-Canonicalization        relaxed/simple
-
-ExternalIgnoreList      refile:/etc/opendkim/TrustedHosts
-InternalHosts           refile:/etc/opendkim/TrustedHosts
-KeyTable                refile:/etc/opendkim/KeyTable
-SigningTable            refile:/etc/opendkim/SigningTable
-
-Mode                    sv
-PidFile                 /var/run/opendkim/opendkim.pid
-SignatureAlgorithm      rsa-sha256
-
-UserID                  opendkim:opendkim
-
-Socket                  inet:12301@localhost
-EOF
-cat >> /etc/default/opendkim <<EOF
-SOCKET="inet:12301@localhost"
-EOF
-
-cat >> /etc/opendkim/TrustedHosts <<EOF
-127.0.0.1
-localhost
-192.168.0.1/24
-
-*.$maildomain
-EOF
-cat >> /etc/opendkim/KeyTable <<EOF
-mail._domainkey.$maildomain $maildomain:mail:$(find /etc/opendkim/domainkeys -iname *.private)
-EOF
-cat >> /etc/opendkim/SigningTable <<EOF
-*@$maildomain mail._domainkey.$maildomain
-EOF
-chown opendkim:opendkim $(find /etc/opendkim/domainkeys -iname *.private)
-chmod 400 $(find /etc/opendkim/domainkeys -iname *.private)
+# postconf -e some_setting=val
